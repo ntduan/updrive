e9be1e3c29b36726ff149e4acb4ce4a9dab7e7e4
nsso <nsso@foxmail.com>
2017-10-25 18:56:24 +0800

    :sparkles: icon

--- src/renderer/components/view/MainMenu.vue
+++ src/renderer/components/view/MainMenu.vue
@@ -2,10 +2,16 @@
   <aside class="menu">
     <ul class="menu-list">
       <li>
-        <a class="is-active">目录</a>
+        <a class="is-active">
+          <svg class="svg-icon"><use xlink:href="#icon-logo"></use></svg>
+          目录
+        </a>
       </li>
       <li>
-        <a>自定义域名</a>
+        <a>
+          <svg class="svg-icon"><use xlink:href="#icon-domain"></use></svg>
+          自定义域名
+        </a>
       </li>
     </ul>
   </aside>
