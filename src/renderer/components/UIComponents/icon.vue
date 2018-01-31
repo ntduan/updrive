36a0e3a7e43792b09caa9c603014775f7b0c2cc8
yznt <nsso@foxmail.com>
2018-01-31 20:22:44 +0800

    :bug: add ui components

---
+++ src/renderer/components/UIComponents/icon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="svg-icon"><use :xlink:href="iconName"></use></svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconName() {
+      return `#${this.name}`
+    },
+  },
+}
+</script>
