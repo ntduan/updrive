c9c17098dbf59e340273fe1b99e5160a85569f90
yznt <nsso@foxmail.com>
2018-01-31 20:36:55 +0800

    :bug: rename

---
+++ src/renderer/components/uiComponents/progressBar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div ref="progressBar"></div>
+</template>
+
+<script>
+import ProgressBar from 'progressbar.js'
+
+export default {
+  name: 'ProgressBar',
+  data() {
+    return {
+      shape: null,
+    }
+  },
+  props: {
+    progress: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  watch: {
+    progress(val) {
+      this.progressAnimate(val)
+    },
+  },
+  destroyed() {
+    this.shape && this.shape.destroy()
+  },
+  mounted() {
+    this.shape = new ProgressBar.Circle(this.$refs.progressBar, {
+      strokeWidth: 14,
+      color: '#2389d0',
+      trailColor: '#aaa',
+      trailWidth: 12,
+    })
+    this.progressAnimate(this.progress)
+  },
+  methods: {
+    progressAnimate(progress = 0) {
+      if (!this.shape) return
+      this.shape.animate(progress)
+    },
+  },
+}
+</script>
